6918ec69830d863bc92630707b35d50d155d7b96
Vasyl Hryts <vasylhryts@knu.ua>
2022-03-25 12:24:01 +0200

    add  input to name and value , create this object

--- client/frontend.js
+++ client/frontend.js
@@ -1,3 +1,26 @@
 import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.esm.browser.js'
 
-console.log(Vue)
+new Vue({
+  el: '#app',
+  data() {
+    return {
+      form: {
+        name: '',
+        value: ''
+      },
+      contacts: []
+    }
+  },
+  methods: {
+    createContact() {
+      const {
+        ...contact
+      } = this.form
+      this.contacts.push({
+        ...contact,
+        id: Date.now()
+      })
+      this.form.name = this.form.value = ''
+    }
+  }
+})
