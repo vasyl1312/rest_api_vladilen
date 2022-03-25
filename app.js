e38121eb3d4094937c3cedf8dd160792dc0e7193
Vasyl Hryts <vasylhryts@knu.ua>
2022-03-25 16:12:59 +0200

    add remove, get data from json base

--- app.js
+++ app.js
@@ -1,6 +1,52 @@
 const express = require('express')
 const path = require('path')
+const {
+  v4
+} = require('uuid')
 const app = express()
+
+let CONTACTS = [{ //like DataBase
+  id: v4(),
+  name: 'Vasyl',
+  value: '+380987654321',
+  marked: false
+}]
+
+app.use(express.json())
+
+// GET
+app.get('/api/contacts', (req, res) => {
+  setTimeout(() => {
+    res.status(200).json(CONTACTS)
+  }, 1000)
+})
+
+// POST
+app.post('/api/contacts', (req, res) => {
+  const contact = {
+    ...req.body,
+    id: v4(),
+    marked: false
+  }
+  CONTACTS.push(contact)
+  res.status(201).json(contact)
+})
+
+// DELETE
+app.delete('/api/contacts/:id', (req, res) => {
+  CONTACTS = CONTACTS.filter(c => c.id !== req.params.id)
+  res.status(200).json({
+    message: 'Контакт був видалений'
+  })
+})
+
+// PUT
+app.put('/api/contacts/:id', (req, res) => {
+  const idx = CONTACTS.findIndex(c => c.id === req.params.id)
+  CONTACTS[idx] = req.body
+  res.json(CONTACTS[idx])
+})
+
 
 app.use(express.static(path.resolve(__dirname, 'client')))
 
@@ -8,6 +54,4 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'client', 'index.html'))
 })
 
-app.listen(3000, () => {
-  console.log('Server has been started on port 3000...')
-})
+app.listen(3000, () => console.log('Server has been started on port 3000...'))
--- client/frontend.js
+++ client/frontend.js
@@ -1,19 +1,26 @@
-import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.esm.browser.js'
+import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.esm.browser.js'
+
+
+Vue.component('loader', {
+  template: `
+    <div style="display: flex;justify-content: center;align-items: center">
+      <div class="spinner-border" role="status">
+        <span class="sr-only">Loading...</span>
+      </div>
+    </div>
+  `
+})
 
 new Vue({
   el: '#app',
   data() {
     return {
+      loading: false,
       form: {
         name: '',
         value: ''
       },
-      contacts: [{
-        id: 1,
-        name: 'Василь',
-        value: '+380987654321',
-        marked: false
-      }]
+      contacts: []
     }
   },
   computed: {
@@ -22,22 +29,54 @@ new Vue({
     }
   },
   methods: {
-    createContact() {
+    async createContact() {
       const {
         ...contact
       } = this.form
-      this.contacts.push({
-        ...contact,
-        id: Date.now()
-      })
+
+      const newContact = await request('/api/contacts', 'POST', contact)
+
+      this.contacts.push(newContact)
+
       this.form.name = this.form.value = ''
     },
-    markContact(id) {
+    async markContact(id) {
       const contact = this.contacts.find(c => c.id === id)
-      contact.marked = true
+      const updated = await request(`/api/contacts/${id}`, 'PUT', {
+        ...contact,
+        marked: true
+      })
+      contact.marked = updated.marked
     },
-    removeContact(id) {
-
+    async removeContact(id) {
+      await request(`/api/contacts/${id}`, 'DELETE')
+      this.contacts = this.contacts.filter(c => c.id !== id)
     }
+  },
+  async mounted() { // start, when ready
+    this.loading = true
+    this.contacts = await request('/api/contacts')
+    this.loading = false
   }
 })
+
+async function request(url, method = 'GET', data = null) {
+  try {
+    const headers = {}
+    let body
+
+    if (data) {
+      headers['Content-Type'] = 'application/json'
+      body = JSON.stringify(data)
+    }
+
+    const response = await fetch(url, {
+      method,
+      headers,
+      body
+    })
+    return await response.json()
+  } catch (e) {
+    console.warn('Error:', e.message)
+  }
+}
